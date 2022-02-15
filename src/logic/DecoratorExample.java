1f1311f32c4462d8a27a2f9efc6b12d5f9b42d7e
AleDiFi <alessandrodifilippo98@gmail.com>
2022-02-15 13:16:28 +0100

    small changes

--- src/logic/DecoratorExample.java
+++ src/logic/DecoratorExample.java
@@ -7,24 +7,25 @@ public class DecoratorExample {
 
     public static void main(String[] args) {
 
+        String whoAreYou = "Who are you?";
         Logger logger = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
 
         Employee thisWillBeBoss = new Engineer( " John Doe",
                 " Programming Department" );
-        logger.log(Level.SEVERE, "Who are you?");
+        logger.log(Level.SEVERE, whoAreYou);
         thisWillBeBoss.whoAmI();
 
         thisWillBeBoss = new AdministrativeManager( thisWillBeBoss );
-        logger.log(Level.SEVERE, "Who are you?");
+        logger.log(Level.SEVERE, whoAreYou);
         thisWillBeBoss.whoAmI();
 
         thisWillBeBoss = new ProjectManager( thisWillBeBoss, "Project ISPW" );
-        logger.log(Level.SEVERE, "Who are you now?");
+        logger.log(Level.SEVERE, whoAreYou);
         thisWillBeBoss.whoAmI();
 
         thisWillBeBoss = new ProjectManager( thisWillBeBoss,
                 "Project DB" );
-        logger.log(Level.SEVERE, "Who are you now?");
+        logger.log(Level.SEVERE, whoAreYou);
         thisWillBeBoss.whoAmI();
     }
 
